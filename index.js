24d737f3bcb140930d0134065f73763c8e043ab4
kayla <kayladatte@gmail.com>
2023-07-20 16:39:31 -0700

    pseudocode

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
-
+// add a package that will import generate file
+// export from "badmath" to import we will use require key word ex: const badmath = require('./badmath.js'/);
+// use your own word to name the variable (not matching badmath)
+// node.js will be executed
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // create readme
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // ask the user questions
+    // we need to save the answers, maybe as a one object
+    // create the content from the responses, combine them into big string, 
+    // call generateMarkdown() function (call it), pass in the perimeter keys
+    // call writeToFile()
+}
 
 // Function call to initialize app
 init();
+
+// create a variable ex: result = init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,10 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+// // inject content using the object keys, can use markdown language
+// `;
+// }
 
 module.exports = generateMarkdown;
